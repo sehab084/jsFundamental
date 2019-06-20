e41e421eee11e8504c9535db74d34aeb19e0f976
Moheuddin Sehab <moheuddinsehab0@gmail.com>
2019-06-20 23:35:31 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// DATA TYPES - String, Number, Boolean, null, undefined
+const name = "Brad";
+const age = 37;
+const rating = 3.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; // undefined
+
+// Check type
+console.log(typeof rating);
